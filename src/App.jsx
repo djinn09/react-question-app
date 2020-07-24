111b38458b3547c8fe4f618ab8df929e85beae38
krunal <krunalh321@gmail.com>
2020-07-24 14:24:14 +0530

    Changes in QUestion Component With token

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import { Route } from "react-router-dom";
 import NavBar from "./_components/NavBar";
 import Questions from "./_components/Questions";
 import Question from "./_components/Question";
-// import { useAppState } from "./context";
 
 function App() {
-  // const { appState } = useAppState();
+  
   return (
     <div className="App">
       <div className="container">
         <div className="col-sm-8 col-sm-offset-2">
-          {/* <Login /> */}
           <div>
             <NavBar />
             <Route exact path="/" component={Questions} />
--- src/_components/Questions.jsx
+++ src/_components/Questions.jsx
@@ -1,11 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import ApiInstance from "../api";
+
+async function QuestionService() {
+  try {
+    const response = await ApiInstance.get("/questions");
+    if (response.status === 200) {
+      let responseData = response.data;
+      return Promise.resolve(responseData);
+    }
+    return Promise.reject("Something went wrong");
+  } catch (error) {
+    // console.log(error);
+
+    return Promise.reject("Something went wrong");
+  }
+}
+
 const Questions = (props) => {
-  const [questions, SetQuestion] = useState(null);
+  const [state, SetState] = useState({
+    questions: null,
+    error: null,
+  });
+  const [isLoading, SetLoading] = useState(true);
+
   const loadData = async () => {
-    const questions = (await axios.get("http://localhost:8081/")).data;
-    SetQuestion(questions);
+    try {
+      const ques = await QuestionService();
+      SetState({ questions: ques });
+      SetLoading(false);
+    } catch (error) {
+      console.log(error);
+      SetState({ error: "Something Went Wrong...." });
+    }
   };
   useEffect(() => {
     loadData();
@@ -13,9 +40,10 @@ const Questions = (props) => {
   return (
     <div className="container">
       <div className="row">
-        {questions === null && <p>Loading questions...</p>}
-        {questions &&
-          questions.map((question) => (
+        {state.error ? (
+          <h1>{state.error}</h1>
+        ) : state.questions && state.questions !== null && !isLoading ? (
+          state.questions.map((question) => (
             <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
               <Link to={`/question/${question.id}`}>
                 <div className="card text-white bg-success mb-3">
@@ -27,7 +55,10 @@ const Questions = (props) => {
                 </div>
               </Link>
             </div>
-          ))}
+          ))
+        ) : (
+          <p>Loading questions...</p>
+        )}
       </div>
     </div>
   );
